165b4b6376ea8e147a8882cd58fb45fb33c9ebd7
Administrator <administrator@Administrators-MacBook-Pro.local>
2020-01-05 19:56:05 +0200

    fetch stub data example.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './index.css';
 import numeral from 'numeral';
+import {getUsers} from './api/userApi';
+
+getUsers().then(result => {
+    let userBody = "";
+
+    result.forEach(user => {
+        userBody+= `<tr>
+        <td>---</td>
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = userBody;
+});
 
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(courseValue);
+
